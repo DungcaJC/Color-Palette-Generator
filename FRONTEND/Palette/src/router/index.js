fc815806c48f13d57a3665046c269f89e4c098db
DungcaJC <JCMDUNGCA@gmail.com>
2026-04-28 22:20:27 +0800

    deploy update

--- FRONTEND/Palette/src/router/index.js
+++ FRONTEND/Palette/src/router/index.js
@@ -15,11 +15,6 @@ const routes = [
   { path: '/save-palette',   component: SavePalette         },
 ]
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 export default createRouter({
   history: createWebHistory(),
   routes,
