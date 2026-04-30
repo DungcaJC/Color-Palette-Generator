1e7d42f99808b7fa6200ed22f1726cc8927677e8
DungcaJC <JCMDUNGCA@gmail.com>
2026-04-30 18:15:48 +0800

    Deploy vercel

--- BACKEND/config/cors.php
+++ BACKEND/config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://your-app-name.vercel.app',
+        'https://color-palette-generator-cg1w.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
