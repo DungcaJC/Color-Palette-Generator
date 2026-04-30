21cef63d49ebdd5e7e57203d3d600f1ed919d64d
DungcaJC <JCMDUNGCA@gmail.com>
2026-04-30 16:24:18 +0800

    Deploy vercel

--- FRONTEND/Palette/vite.config.js
+++ FRONTEND/Palette/vite.config.js
@@ -3,14 +3,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-  
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),],
+  plugins: [vue(), tailwindcss()],
   server: {
     port: 5173,
-    strictPort: true, // ← forces 5173, errors if taken
+    strictPort: true,
+  },
+  css: {
+    transformer: 'postcss'  // ← moved here
   }
 })
 
